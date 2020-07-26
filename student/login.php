f13a411f77e9bac79e642b39e9e3ca16adba32d4
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-07-26 09:05:57 +0530

    Student login done

--- student/login.php
+++ student/login.php
@@ -1,5 +1,5 @@
 <?php
-
+include "connection.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -65,10 +65,31 @@
 
 </div>
 
-<div class="alert alert-danger col-lg-6 col-lg-push-3">
-    <strong style="color:white">Invalid</strong> Username Or Password.
-</div>
+<?php
+if(isset($_POST["submit1"]))
+{
+    $count = 0;
+    $res= mysqli_query($link,"select * from student_registration where username='$_POST[username]' && password='$_POST[password]'");
+    $count = mysqli_num_rows($res);
+    
 
-
+    if($count==0)
+    {
+        ?>
+        <div class="alert alert-danger col-lg-6 col-lg-push-3">
+            <strong style="color:white">Invalid</strong> Username Or Password.
+        </div>
+<?php
+    }
+    else
+    {
+        ?>
+<script type="text/javascript">
+window.location="location.php";
+</script>
+<?php
+    }
+}
+?>
 </body>
 </html>
